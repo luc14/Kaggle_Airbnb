3af97a0b04d00868978ebfd8e04235ef67ab5bf9
Lu <luc14.uci@gmail.com>
2016-01-28 12:23:08 -0800

    airbnb, first test(score: 0.86141), add submission file

--- airbnb/airbnb.py
+++ airbnb/airbnb.py
@@ -23,13 +23,29 @@ first_device_type: c
 first_browser: c
 country_destination: target
 '''    
-
-X, y, X_test = common.prepare_data('data/train_users_2.csv','data/test_users.csv', info_str)
-
-logreg = LogisticRegression(random_state=1)
-#logreg_score = common.evaluate(logreg, X, y)
-logreg.fit(X, y)
-
-prob = logreg.predict_proba(X_test)
-print( X, y, X_test, prob, file = open('out.txt','w'), sep= '\n' )
-
+def main():
+    X, y, X_test = common.prepare_data('data/train_users_2.csv','data/test_users.csv', info_str)
+    
+    logreg = LogisticRegression(random_state=1)
+    #logreg_score = common.evaluate(logreg, X, y)
+    logreg.fit(X, y)
+    
+    
+    #print( X, y, X_test, prob, file = open('out.txt','w'), sep= '\n' )
+    output_result(logreg, X_test, 'submission.csv')
+    
+def output_result(learner, X_test, filename):
+#def output_result(prob, index_id, countries):
+    countries = learner.classes_
+    prob = learner.predict_proba(X_test)
+    data = pd.DataFrame.from_records(prob, index=X_test.index, columns = countries)
+    file = open(filename,'w')
+    print('id,country', file = file)
+    for user_id in data.index:
+        top_countries  = sorted(countries, key = lambda country: data.loc[user_id,country],reverse=True)[:5]
+        for i in range(5):
+            print(user_id, top_countries[i], file= file, sep=',')
+  
+if __name__ == '__main__':
+    main()  
+    
